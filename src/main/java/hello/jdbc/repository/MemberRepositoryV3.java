51b14bd921bed7344ba51d37b76a33813d210993
KIM <stonehead334@gmail.com>
2022-08-08 16:03:33 +0900

    트랜잭션 문제 해결 - 트랜잭션 매니저1

---
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV3.java
@@ -0,0 +1,152 @@
+package hello.jdbc.repository;
+
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/**
+ * 트랜잭션 - 트랜잭션 메니저
+ * DataSourceUtils.getConnection()
+ * DataSourceUtils.releaseConnection()
+ */
+
+
+@Slf4j
+public class MemberRepositoryV3 {
+
+
+    private final DataSource dataSource;
+
+    public MemberRepositoryV3(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public Member save(Member member) throws SQLException {
+
+        // sql문 작성
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            // 외부 리소스를 쓰는 거라 닫아줘야 한다
+            // 둘 중 하나라도 오류가 난다면 뒤에 있는 함수가 실행이 안되서 함수로 뽑았다
+            close(con, pstmt, null);
+        }
+    }
+
+    //조회하기
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            //next 실행시 다음 함수가 시작된다
+            rs = pstmt.executeQuery();
+            if(rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else  { // 못 찾으면 실행
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch( SQLException e)  {
+            log.error("db erorr", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+
+            // execute 는 쿼리를 실행하고 영향받은 row수를 반환
+            int resultSize =  pstmt.executeUpdate();
+            log.info("resultSize={}", resultSize);
+
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            // 외부 리소스를 쓰는 거라 닫아줘야 한다
+            // 둘 중 하나라도 오류가 난다면 뒤에 있는 함수가 실행이 안되서 함수로 뽑았다
+            close(con, pstmt, null);
+        }
+    }
+
+
+
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(stmt);
+
+        //주의: 트랜잭션 동기화를 사용하려면 DataSourceUtils를 써야 한다
+        DataSourceUtils.releaseConnection(con, dataSource);
+        //JdbcUtils.closeConnection(con);
+    }
+
+
+    private Connection getConnection() throws SQLException {
+
+        //주의: 트랜잭션 동기화를 사용하려면 DataSourceUtils를 써야 한다
+        Connection con = DataSourceUtils.getConnection(dataSource);
+        log.info("get Connection={}, class={}", con, con.getClass());
+        return con;
+    }
+
+}
---
+++ src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,71 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV2;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *  트랜잭션 - 트랜잭션 매니저
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_1 {
+
+    //private final DataSource dataSource;
+    private final PlatformTransactionManager transactionManager;
+    private final MemberRepositoryV3 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        //트랜잭션 시작
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+
+        try {
+            //트랜잭션 시작
+            bizLogic( fromId, toId, money);
+            transactionManager.commit(status);// 성공 시 커밋
+        } catch (Exception e) {
+            // 실패하면 롤백
+            transactionManager.rollback(status);
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        //비즈니스 로직
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        // 실제 계좌이체처럼 한곳에서 빠지고 한 곳은 돈이 들어간다
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void release(Connection con) {
+        if(con != null) {
+            try {
+                con.setAutoCommit(true);
+                con.close();
+            } catch (Exception e)  {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex"))
+            throw new IllegalStateException("계좌이체 중 예외 발생");
+    }
+}
