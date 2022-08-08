1439b31233f73703bc9d9725eea78cd7e891bd20
KIM <stonehead334@gmail.com>
2022-08-08 16:44:28 +0900

    트랜잭션 문제 해결 - 트랜잭션 탬플릿

---
+++ src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -0,0 +1,74 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *  트랜잭션 - 트랜잭션 템플릿
+ */
+@Slf4j
+public class MemberServiceV3_2 {
+
+
+    //private final PlatformTransactionManager transactionManager;
+    private final TransactionTemplate txTemplate;
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        txTemplate.executeWithoutResult((status) -> {
+            //비즈니스 로직
+            try {
+                bizLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                //비즈니스 로직이 정상 수행되면 커밋한다.
+                //언체크 예외가 발생하면 롤백한다. 그 외의 경우 커밋한다
+                throw new IllegalStateException(e);
+            }
+            //코드에서 예외를 처리하기 위해 try~catch 가 들어갔는데, bizLogic() 메서드를 호출하면
+            //SQLException 체크 예외를 넘겨준다. 해당 람다에서 체크 예외를 밖으로 던질 수 없기 때문에 언체크
+            //예외로 바꾸어 던지도록 예외를 전환했다
+        });
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        //비즈니스 로직
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        // 실제 계좌이체처럼 한곳에서 빠지고 한 곳은 돈이 들어간다
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void release(Connection con) {
+        if(con != null) {
+            try {
+                con.setAutoCommit(true);
+                con.close();
+            } catch (Exception e)  {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex"))
+            throw new IllegalStateException("계좌이체 중 예외 발생");
+    }
+}
